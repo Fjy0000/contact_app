c36a6f827370933ab779c7b7b085b1dbff5453e3
JUNYI <fongjunyi11@gmail.com>
2023-06-20 16:44:52 +0800

    [Update] : loading dialog, text type

--- lib/modules/contact/viewModel/create_contact_viewmodel.dart
+++ lib/modules/contact/viewModel/create_contact_viewmodel.dart
@@ -5,40 +5,88 @@ import 'package:app2/base/base_viewmodel.dart';
 import 'package:app2/main.dart';
 import 'package:app2/model/body/contact_body.dart';
 import 'package:app2/utils/constants/enums.dart';
+import 'package:app2/utils/dialog_util.dart';
 import 'package:app2/utils/extension.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 
 class CreateContactViewModel extends BaseViewModel {
   Future<void> saveContact(ContactBean contact) async {
+    final contactDoc = collectionReference.doc();
+    final refUploadAvatarName = storageReference.child(contactDoc.id);
+
+    bool _isLoadingDialogShown = false;
     String avatarUrl = '';
 
-    final contactDoc = collectionReference.doc();
-
     if (contact.imagePath != '') {
-      final refUploadAvatarName = storageReference.child(contactDoc.id);
-      await refUploadAvatarName.putFile(File("${contact.imagePath}"));
-      avatarUrl = await refUploadAvatarName.getDownloadURL();
+      await refUploadAvatarName
+          .putFile(File("${contact.imagePath}"))
+          .snapshotEvents
+          .listen((taskSnapshot) async {
+        switch (taskSnapshot.state) {
+          case TaskState.running:
+            if (!_isLoadingDialogShown) {
+              showLoadingDialog();
+              _isLoadingDialogShown = true;
+            }
+            break;
+          case TaskState.paused:
+            // TODO: Handle this case.
+            break;
+          case TaskState.canceled:
+            // TODO: Handle this case.
+            break;
+          case TaskState.error:
+            showToast('save contact fail');
+            break;
+          case TaskState.success:
+            dismissDialog();
+
+            avatarUrl = await refUploadAvatarName.getDownloadURL();
+            final saveData = ContactBean(
+              id: contactDoc.id,
+              name: contact.name,
+              contactNo: contact.contactNo,
+              organisation: contact.organisation,
+              email: contact.email,
+              address: contact.address,
+              note: contact.note,
+              imagePath: avatarUrl,
+            );
+
+            final json = saveData.toJson();
+
+            await contactDoc.set(json).request(
+              onSuccess: (value) {
+                showToast("Successfully Added!");
+                eventBus?.fire(BaseEventBus(EventBusAction.REFRESH_CONTACT));
+                Get.back();
+              },
+            );
+            break;
+        }
+      });
+    } else {
+      final saveData = ContactBean(
+        id: contactDoc.id,
+        name: contact.name,
+        contactNo: contact.contactNo,
+        organisation: contact.organisation,
+        email: contact.email,
+        address: contact.address,
+        note: contact.note,
+        imagePath: avatarUrl,
+      );
+
+      final json = saveData.toJson();
+
+      await contactDoc.set(json).request(
+        onSuccess: (value) {
+          showToast("Successfully Added!");
+          eventBus?.fire(BaseEventBus(EventBusAction.REFRESH_CONTACT));
+          Get.back();
+        },
+      );
     }
-
-    final saveData = ContactBean(
-      id: contactDoc.id,
-      name: contact.name,
-      contactNo: contact.contactNo,
-      organisation: contact.organisation,
-      email: contact.email,
-      address: contact.address,
-      note: contact.note,
-      imagePath: avatarUrl,
-    );
-
-    final json = saveData.toJson();
-
-    await contactDoc.set(json).request(
-      onSuccess: (value) {
-        showToast("Successfully Added!");
-        eventBus?.fire(BaseEventBus(EventBusAction.REFRESH_CONTACT));
-        Get.back();
-      },
-    );
   }
 }
--- lib/modules/contact/viewModel/edit_contact_viewmodel.dart
+++ lib/modules/contact/viewModel/edit_contact_viewmodel.dart
@@ -5,6 +5,7 @@ import 'package:app2/base/base_viewmodel.dart';
 import 'package:app2/main.dart';
 import 'package:app2/model/body/contact_body.dart';
 import 'package:app2/utils/constants/enums.dart';
+import 'package:app2/utils/dialog_util.dart';
 import 'package:app2/utils/extension.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
@@ -12,27 +13,65 @@ import 'package:get/get.dart';
 class EditContactViewModel extends BaseViewModel {
   Future<void> editContact(bool? isChangeAvatar, ContactBean contact) async {
     String avatarUrl = contact.imagePath ?? '';
+    bool _isLoadingDialogShown = false;
 
     final contactDoc = collectionReference.doc(contact.id);
+    final refUploadAvatarName = storageReference.child("${contact.id}");
 
     if (isChangeAvatar == true) {
-      final refUploadAvatarName = storageReference.child("${contact.id}");
-      refUploadAvatarName.putFile(File("${contact.imagePath}"));
-      avatarUrl = await refUploadAvatarName.getDownloadURL();
-    }
+      refUploadAvatarName
+          .putFile(File("${contact.imagePath}"))
+          .snapshotEvents
+          .listen((taskSnapshot) async {
+        switch (taskSnapshot.state) {
+          case TaskState.running:
+            if (!_isLoadingDialogShown) {
+              showLoadingDialog();
+              _isLoadingDialogShown = true;
+            }
+            break;
+          case TaskState.paused:
+            // TODO: Handle this case.
+            break;
+          case TaskState.canceled:
+            // TODO: Handle this case.
+            break;
+          case TaskState.error:
+            showToast('save contact fail');
+            break;
+          case TaskState.success:
+            dismissDialog();
 
-    await contactDoc.update({
-      'name': contact.name,
-      'contactNo': contact.contactNo,
-      'organisation': contact.organisation,
-      'email': contact.email,
-      'address': contact.address,
-      'note': contact.note,
-      'imagePath': avatarUrl,
-    }).request(onSuccess: (value) {
-      showToast("Successfully Updated!");
-      eventBus?.fire(BaseEventBus(EventBusAction.REFRESH_CONTACT));
-      Get.back();
-    });
+            avatarUrl = await refUploadAvatarName.getDownloadURL();
+            await contactDoc.update({
+              'name': contact.name,
+              'contactNo': contact.contactNo,
+              'organisation': contact.organisation,
+              'email': contact.email,
+              'address': contact.address,
+              'note': contact.note,
+              'imagePath': avatarUrl,
+            }).request(onSuccess: (value) {
+              showToast("Successfully Updated!");
+              eventBus?.fire(BaseEventBus(EventBusAction.REFRESH_CONTACT));
+              Get.back();
+            });
+            break;
+        }
+      });
+    } else {
+      await contactDoc.update({
+        'name': contact.name,
+        'contactNo': contact.contactNo,
+        'organisation': contact.organisation,
+        'email': contact.email,
+        'address': contact.address,
+        'note': contact.note,
+      }).request(onSuccess: (value) {
+        showToast("Successfully Updated!");
+        eventBus?.fire(BaseEventBus(EventBusAction.REFRESH_CONTACT));
+        Get.back();
+      });
+    }
   }
 }
